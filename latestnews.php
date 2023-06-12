40d473c3b70391bcc810bf8124e80f00cf0ba070
YangzeeSh <yeanjeechherpa@gmail.com>
2023-06-12 06:51:21 +0545

    update all

--- latestnews.php
+++ latestnews.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,125 +9,139 @@
     include "./links.php";
     ?>
 </head>
+
 <body>
     <?php
     include "./header.php";
     ?>
 
-<div class="sports">
+
+
+    <div class="news-nav">
+
+        <img src="img/bg 3.jpg">
+
+    </div>
+
+
+    <div class="sports">
         <div class="nav">
             <a href="news.php">Top News</a>
             <a href="latestnews.php">Latest News</a>
-            
-        </div> 
+
         </div>
+    </div>
 
 
 
-<main>
-            <section class = "main-container-left">
-                <h2>Top Stories</h2>
-                <div class = "container-top-left">
-                    <article>
-                        <img src = "img/adidas football.webp">
+    <main>
 
-                        <div>
-                            <h3>Best Used Cars Under $20, 000 for families</h3>
-
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ea sint, nisi rem earum fugit? Facere veritatis sapiente eveniet quibusdam.</p>
-
-                            <a href = "#">Read More <span>>></span></a>
-                        </div>
-                    </article>
+        <section class="main-container-left">
+            <div class="sports">
+                <div class="nav">
+                    <a href="#Topstories">Top Stories</a>
                 </div>
+            </div>
+            <div class="container-top-left">
+                <article>
+                    <img src="img/adidas football.webp" style="width: 400px; height: 400px;">
 
-                <div class = "container-bottom-left">
-                    <article>
-                        <img src = "img/bg 1.jpg">
-                        <div>
-                            <h3>Best smart speakers for the year</h3>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi iure modi animi cupiditate. Explicabo, nihil?</p>
+                    <div>
+                        <h3>JACK GREALISH REVELS IN MAN CITY CHAMPIONS LEAGUE WIN - ‘IT’S WHAT YOU WORK YOUR WHOLE LIFE FOR’</h3>
 
-                            <a href = "#">Read More <span>>></span></a>
-                        </div>
-                    </article>
+                        <p>Manchester City winger Jack Grealish suggested he had been one of his team's worst performers on Saturday night
+                            as they defeated Inter Milan 1-0 in Istanbul to secure the Champions League and Treble. However he, like Rodri and
+                            Ilkay Gundogan, was elated to
+                            have secured the three most prestigious trophies on offer to them at the start of the season.Manchester City winger Jack Grealish was full of pride as he celebrated a 1-0 win
+                            over Inter Milan in the Champions League final.
+                            Grealish and his team-mates became just the second Premier League side to win the Treble, having already secured the Premier League title and FA Cup over the last few weeks.
+                            The former Aston Villa player has been one of the standout performers at times during the campaign for Pep Guardiola’s men, though speaking to BT Sport after the match, he believed he had been off the boil against Inter.
+                            <a href="#">Read More <span>>></span></a>
+                    </div>
+                </article>
+            </div>
 
-                    <article>
-                        <img src = "img/Affordable At-Home Exercise Equipment for a Total Body Sweat Session 1.png">
-                        <div>
-                            <h3>iPad Pro, reviewed: Has the iPad become my main computer now?</h3>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi iure modi animi cupiditate. Explicabo, nihil?</p>
+            <div class="container-top-left">
+                <article>
+                    <img src="img/adidas football.webp">
 
-                            <a href = "#">Read More <span>>></span></a>
-                        </div>
-                    </article>
+                    <div>
+                        <h3>Best Used Cars Under $20, 000 for families</h3>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ea sint, nisi rem earum fugit? Facere veritatis sapiente eveniet quibusdam.</p>
+
+                        <a href="#">Read More <span>>></span></a>
+                    </div>
+                </article>
+            </div>
+
+
+        </section>
+
+        <section class="main-container-right">
+            <h2>Latest Stories</h2>
+
+            <article>
+                <h4>just in </h4>
+                <div>
+                    <h2>Here's how to track your stimulus check with the IRS Get My Payment Portal</h2>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus?</p>
+
+                    <a href="#">Read More <span>>></span></a>
                 </div>
-            </section>
+                <img src="img/black jersey.jpeg">
+            </article>
 
-<section class = "main-container-right">
-                <h2>Latest Stories</h2>
-                
-                <article>
-                    <h4>just in </h4>
-                    <div>
-                        <h2>Here's how to track your stimulus check with the IRS Get My Payment Portal</h2>
+            <article>
+                <h4>just in </h4>
+                <div>
+                    <h2>The best outdoor games to play with your family</h2>
 
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus?</p>
 
-                        <a href = "#">Read More <span>>></span></a>
-                    </div>
-                    <img src = "img/black jersey.jpeg">
-                </article>
+                    <a href="#">Read More <span>>></span></a>
+                </div>
+                <img src="img/news 6.jpeg">
+            </article>
 
-                <article>
-                    <h4>just in </h4>
-                    <div>
-                        <h2>The best outdoor games to play with your family</h2>
+            <article>
+                <h4>just in </h4>
+                <div>
+                    <h2>Why walk? Check out the best electric scooters and e-bikes for 2020</h2>
 
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus?</p>
 
-                        <a href = "#">Read More <span>>></span></a>
-                    </div>
-                    <img src = "img/news 6.jpeg">
-                </article>
+                    <a href="#">Read More <span>>></span></a>
+                </div>
+                <img src="images/right-3.jpg">
+            </article>
 
-                <article>
-                    <h4>just in </h4>
-                    <div>
-                        <h2>Why walk? Check out the best electric scooters and e-bikes for 2020</h2>
+            <article>
+                <h4>just in </h4>
+                <div>
+                    <h2>Disneyland Paris will stream its Lion King stage show Friday night</h2>
 
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus?</p>
 
-                        <a href = "#">Read More <span>>></span></a>
-                    </div>
-                    <img src = "images/right-3.jpg">
-                </article>
+                    <a href="#">Read More <span>>></span></a>
+                </div>
+                <img src="images/right-4.jpg">
+            </article>
 
-                <article>
-                    <h4>just in </h4>
-                    <div>
-                        <h2>Disneyland Paris will stream its Lion King stage show Friday night</h2>
+            <article>
+                <h4>just in </h4>
+                <div>
+                    <h2>Looking at a phone's lock screen also requries a warrant, judge rules</h2>
 
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus?</p>
 
-                        <a href = "#">Read More <span>>></span></a>
-                    </div>
-                    <img src = "images/right-4.jpg">
-                </article>
-
-                <article>
-                    <h4>just in </h4>
-                    <div>
-                        <h2>Looking at a phone's lock screen also requries a warrant, judge rules</h2>
-
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, repellendus?</p>
-
-                        <a href = "#">Read More <span>>></span></a>
-                    </div>
-                    <img src = "images/right-5.jpg">
-                </article>
-            </section>
-        </main>
+                    <a href="#">Read More <span>>></span></a>
+                </div>
+                <img src="images/right-5.jpg">
+            </article>
+        </section>
+    </main>
 
 
 
@@ -144,4 +159,5 @@
     include "./footer.php";
     ?>
 </body>
+
 </html>
--- news.php
+++ news.php
@@ -6,15 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Read More Read Less Card</title>
     <link rel="stylesheet" href="css/news.css">
-   <?php
-   include "./links.php";
-   ?>
+    <?php
+    include "./links.php";
+    ?>
 </head>
 
 <body>
     <?php
     include "./header.php";
     ?>
+
+
 
     <div class="news-nav">
 
@@ -27,9 +29,9 @@
         <div class="nav">
             <a href="news.php">Top News</a>
             <a href="latestnews.php">Latest News</a>
-            
-        </div> 
+
         </div>
+    </div>
     <div class="card-holder">
 
         <div class="card">
@@ -202,7 +204,7 @@
 
         </div>
 
-        
+
 
 
 
